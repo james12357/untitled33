fcb13b06547d2173ce139790ccf758c6f2df11ea
c2008 <c2008@DESKTOP-N2AQK1M>
2021-05-09 11:17:21 +0800

    q

--- newtype.py
+++ newtype.py
@@ -1,24 +1,28 @@
-import hashlib
+
 
 
 class bank:
     def __init__(self):
-        self.__cards = {90000: ["8cb2237d0679ca88db6464eac60da96345513964", 100]}
+        self.__cards = {"90000": ["12345", 100]}
         self.__passwd = 0
 
     def __log(self, message):
         pass
 
     def login(self):
-        self.__card = input("卡号>")
+        self.__card = str(input("卡号>"))
         self.__passwd = input("密码>")
         if self.__card in self.__cards:
-            if hashlib.new("sha1", self.__passwd.encode("utf-8")) == self.__cards[self.__card][0]:
+            if self.__passwd == self.__cards[self.__card][0]:
                 self.__log(self.__card)
-                return [True, self.__card, self.__cards[self.__card][1]]
+                return [True]
+            else:
+                return [False]
+        else:
+            return [False]
 
     def purchase(self, mone):
-        if "int" in type(mone):
+        if type(1.0) == type(mone):
             self.__cards[self.__card][1] += mone
             self.__log("存款")
             return True
@@ -27,17 +31,27 @@ class bank:
 
     def getmoney(self):
         return self.__cards[self.__card][1]
+    def takemoney(self, amount):
+        if amount < self.getmoney():
+            self.__cards[self.__card][1] -= amount
+            return True
 
 main = bank()
 mess = main.login()
+
 if mess[0]:
     while True:
-        a = int(input("请选择功能：\n1.查询余额\n2.存款\n3.查询"))
+        a = int(input("请选择功能：\n1.查询余额\n2.存款\n3.取款"))
         if a == 1:
-            main.getmoney()
+            print(main.getmoney())
         elif a == 2:
-            if main.purchase(input("存额>")):
-                print("存款成功,当前余额" + main.getmoney())
+            tmp = main.purchase(float(input("存额>")))
+            if tmp:
+                print("存款成功,当前余额" + str(main.getmoney()))
+        elif a == 3:
+            if main.takemoney(float(input("取额"))):
+                print("取钱成功")
+
 
 
 
